a42d9ca3359bd067926f06f17ef21154fdd59b82
lucas <lucas.lima716@etec.sp.gov.br>
2024-06-13 21:29:15 -0300

    finalizado talvez

--- excluirItem.php
+++ excluirItem.php
@@ -3,21 +3,7 @@ session_start();
 
 if(isset($_GET['excluir'])){
   $id_pizza = $_GET['excluir'];
-
-  if (isset($_SESSION['carrinho'])) {
-    if (isset($_SESSION['carrinho'][$id_pizza])) {
-      // $_SESSION['carrinho'][$id_pizza] = [
-      //   'quantidade' => 0
-      // ];
-    //} else {
-      $_SESSION['carrinho'][$id_pizza] = [];
-      $_SESSION['carrinho'][$id_pizza]['quantidade']--;
-    //}
+  unset($_SESSION['carrinho'][$id_pizza]);
   }
-}
-}
-echo "<pre>";
-echo var_dump($_SESSION['carrinho']);
-echo "</pre>";
 
-//header("Location: carrinho.php");
+header("Location: carrinho.php");
